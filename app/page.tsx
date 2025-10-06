4e371f4098f044aa524f883520907053c1e38e9d
sabique <sabiqueislam7@gmail.com>
2025-10-06 20:26:38 +0530

    refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 import Navbar from '@/components/Navbar'
-import { Hero } from '@/components/Hero'
 import Footer from '@/components/Footer'
-import HowToUse from '@/components/Description'
+import Generate from '@/components/Generate'
 
 export default function Home() {
   return (
       <div>
         <Navbar />
-        <Hero/>
-        <HowToUse/>
+        <Generate />
         <Footer/>
       </div>
   )
